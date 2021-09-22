7b249a6f2493f44d12016efa2f5d37b4c63a0a7b
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-22 16:50:38 +0900

    基本的なページ遷移を実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import "./styles.css";
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+      {/* どのパスにどのコンポーネントをあてるか記述 */}
+      {/* exactをつけないと/以降すべて/のパスがレンダリングされる
+      exactをつけて完全一致させる */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
